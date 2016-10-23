1b4159344bb2c835e1bb63d6d6d4adcf6ace9432
SantillanaJasmin <jasminsantillana_cse@yahoo.com>
2016-10-24 00:25:47 +0800

    SOM Process

--- src/maclern_som/Example.java
+++ src/maclern_som/Example.java
@@ -10,9 +10,331 @@ package maclern_som;
  * @author Hanna Sha
  */
 public class Example {
-    double attendance, difficulty, Q2, Q3, Q4, Q5, 
+    private double attendance, difficulty, Q2, Q3, Q4, Q5, 
             Q6, Q8, Q9, Q11, Q13, Q14, Q15, Q16, Q17,
             Q18, Q20, Q21, Q22, Q24, Q25, Q27, Q28;
+
+    /**
+     * @return the attendance
+     */
+    public double getAttendance() {
+        return attendance;
+    }
+
+    /**
+     * @return the difficulty
+     */
+    public double getDifficulty() {
+        return difficulty;
+    }
+
+    /**
+     * @return the Q2
+     */
+    public double getQ2() {
+        return Q2;
+    }
+
+    /**
+     * @return the Q3
+     */
+    public double getQ3() {
+        return Q3;
+    }
+
+    /**
+     * @return the Q4
+     */
+    public double getQ4() {
+        return Q4;
+    }
+
+    /**
+     * @return the Q5
+     */
+    public double getQ5() {
+        return Q5;
+    }
+
+    /**
+     * @return the Q6
+     */
+    public double getQ6() {
+        return Q6;
+    }
+
+    /**
+     * @return the Q8
+     */
+    public double getQ8() {
+        return Q8;
+    }
+
+    /**
+     * @return the Q9
+     */
+    public double getQ9() {
+        return Q9;
+    }
+
+    /**
+     * @return the Q11
+     */
+    public double getQ11() {
+        return Q11;
+    }
+
+    /**
+     * @return the Q13
+     */
+    public double getQ13() {
+        return Q13;
+    }
+
+    /**
+     * @return the Q14
+     */
+    public double getQ14() {
+        return Q14;
+    }
+
+    /**
+     * @return the Q15
+     */
+    public double getQ15() {
+        return Q15;
+    }
+
+    /**
+     * @return the Q16
+     */
+    public double getQ16() {
+        return Q16;
+    }
+
+    /**
+     * @return the Q17
+     */
+    public double getQ17() {
+        return Q17;
+    }
+
+    /**
+     * @return the Q18
+     */
+    public double getQ18() {
+        return Q18;
+    }
+
+    /**
+     * @return the Q20
+     */
+    public double getQ20() {
+        return Q20;
+    }
+
+    /**
+     * @return the Q21
+     */
+    public double getQ21() {
+        return Q21;
+    }
+
+    /**
+     * @return the Q22
+     */
+    public double getQ22() {
+        return Q22;
+    }
+
+    /**
+     * @return the Q24
+     */
+    public double getQ24() {
+        return Q24;
+    }
+
+    /**
+     * @return the Q25
+     */
+    public double getQ25() {
+        return Q25;
+    }
+
+    /**
+     * @return the Q27
+     */
+    public double getQ27() {
+        return Q27;
+    }
+
+    /**
+     * @return the Q28
+     */
+    public double getQ28() {
+        return Q28;
+    }
+
+    /**
+     * @param attendance the attendance to set
+     */
+    public void setAttendance(double attendance) {
+        this.attendance = attendance;
+    }
+
+    /**
+     * @param difficulty the difficulty to set
+     */
+    public void setDifficulty(double difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    /**
+     * @param Q2 the Q2 to set
+     */
+    public void setQ2(double Q2) {
+        this.Q2 = Q2;
+    }
+
+    /**
+     * @param Q3 the Q3 to set
+     */
+    public void setQ3(double Q3) {
+        this.Q3 = Q3;
+    }
+
+    /**
+     * @param Q4 the Q4 to set
+     */
+    public void setQ4(double Q4) {
+        this.Q4 = Q4;
+    }
+
+    /**
+     * @param Q5 the Q5 to set
+     */
+    public void setQ5(double Q5) {
+        this.Q5 = Q5;
+    }
+
+    /**
+     * @param Q6 the Q6 to set
+     */
+    public void setQ6(double Q6) {
+        this.Q6 = Q6;
+    }
+
+    /**
+     * @param Q8 the Q8 to set
+     */
+    public void setQ8(double Q8) {
+        this.Q8 = Q8;
+    }
+
+    /**
+     * @param Q9 the Q9 to set
+     */
+    public void setQ9(double Q9) {
+        this.Q9 = Q9;
+    }
+
+    /**
+     * @param Q11 the Q11 to set
+     */
+    public void setQ11(double Q11) {
+        this.Q11 = Q11;
+    }
+
+    /**
+     * @param Q13 the Q13 to set
+     */
+    public void setQ13(double Q13) {
+        this.Q13 = Q13;
+    }
+
+    /**
+     * @param Q14 the Q14 to set
+     */
+    public void setQ14(double Q14) {
+        this.Q14 = Q14;
+    }
+
+    /**
+     * @param Q15 the Q15 to set
+     */
+    public void setQ15(double Q15) {
+        this.Q15 = Q15;
+    }
+
+    /**
+     * @param Q16 the Q16 to set
+     */
+    public void setQ16(double Q16) {
+        this.Q16 = Q16;
+    }
+
+    /**
+     * @param Q17 the Q17 to set
+     */
+    public void setQ17(double Q17) {
+        this.Q17 = Q17;
+    }
+
+    /**
+     * @param Q18 the Q18 to set
+     */
+    public void setQ18(double Q18) {
+        this.Q18 = Q18;
+    }
+
+    /**
+     * @param Q20 the Q20 to set
+     */
+    public void setQ20(double Q20) {
+        this.Q20 = Q20;
+    }
+
+    /**
+     * @param Q21 the Q21 to set
+     */
+    public void setQ21(double Q21) {
+        this.Q21 = Q21;
+    }
+
+    /**
+     * @param Q22 the Q22 to set
+     */
+    public void setQ22(double Q22) {
+        this.Q22 = Q22;
+    }
+
+    /**
+     * @param Q24 the Q24 to set
+     */
+    public void setQ24(double Q24) {
+        this.Q24 = Q24;
+    }
+
+    /**
+     * @param Q25 the Q25 to set
+     */
+    public void setQ25(double Q25) {
+        this.Q25 = Q25;
+    }
+
+    /**
+     * @param Q27 the Q27 to set
+     */
+    public void setQ27(double Q27) {
+        this.Q27 = Q27;
+    }
+
+    /**
+     * @param Q28 the Q28 to set
+     */
+    public void setQ28(double Q28) {
+        this.Q28 = Q28;
+    }
     
 
 }
--- src/maclern_som/Main.java
+++ src/maclern_som/Main.java
@@ -5,12 +5,17 @@
  */
 package maclern_som;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.MathContext;
 import java.util.ArrayList;
-import java.util.Random;
+
+import org.encog.Encog;
+import org.encog.mathutil.matrices.Matrix;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLData;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.som.SOM;
+import org.encog.neural.som.training.basic.BasicTrainSOM;
+import org.encog.neural.som.training.basic.neighborhood.NeighborhoodSingle;
 
 /**
  *
@@ -21,8 +26,131 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) { 
-        SOM som = new SOM();
-        som.SOM();
+    public static double SOM_INPUT1[][] = {
+        {-1.0, -1.0, 1.0, 1.0},
+        {1.0, 1.0, -1.0, -1.0}};
+
+    public static void main(String[] args) {
+//        SOM som = new SOM();
+//        som.SOM();
+
+        //Read CSV file
+        CSVcontroller cc = new CSVcontroller();
+        cc.readCSV("E:\\10th_Term\\MACLERN\\MP1\\MACLERN_Milestone1\\TSE Data Normalized and Cleaned.csv");
+
+        //initialize SOM input
+        ArrayList<Example> samples = cc.getSample();
+        double[][] input = new double[5820][23];
+
+        for (int r = 0; r < input.length; r++) {
+            for (int c = 0; c < input.length; c++) {
+                switch (c) {
+                    case 0:
+                        input[r][c] = samples.get(r).getAttendance();
+                        break;
+                    case 1:
+                        input[r][c] = samples.get(r).getDifficulty();
+                        break;
+                    case 2:
+                        input[r][c] = samples.get(r).getQ2();
+                        break;
+                    case 3:
+                        input[r][c] = samples.get(r).getQ3();
+                        break;
+                    case 4:
+                        input[r][c] = samples.get(r).getQ4();
+                        break;
+                    case 5:
+                        input[r][c] = samples.get(r).getQ5();
+                        break;
+                    case 6:
+                        input[r][c] = samples.get(r).getQ6();
+                        break;
+                    case 7:
+                        input[r][c] = samples.get(r).getQ8();
+                        break;
+                    case 8:
+                        input[r][c] = samples.get(r).getQ9();
+                        break;
+                    case 9:
+                        input[r][c] = samples.get(r).getQ11();
+                        break;
+                    case 10:
+                        input[r][c] = samples.get(r).getQ13();
+                        break;
+                    case 11:
+                        input[r][c] = samples.get(r).getQ14();
+                        break;
+                    case 12:
+                        input[r][c] = samples.get(r).getQ15();
+                        break;
+                    case 13:
+                        input[r][c] = samples.get(r).getQ16();
+                        break;
+                    case 14:
+                        input[r][c] = samples.get(r).getQ17();
+                        break;
+                    case 15:
+                        input[r][c] = samples.get(r).getQ18();
+                        break;
+                    case 16:
+                        input[r][c] = samples.get(r).getQ20();
+                        break;
+                    case 17:
+                        input[r][c] = samples.get(r).getQ21();
+                        break;
+                    case 18:
+                        input[r][c] = samples.get(r).getQ22();
+                        break;
+                    case 19:
+                        input[r][c] = samples.get(r).getQ24();
+                        break;
+                    case 20:
+                        input[r][c] = samples.get(r).getQ25();
+                        break;
+                    case 21:
+                        input[r][c] = samples.get(r).getQ27();
+                        break;
+                    case 22:
+                        input[r][c] = samples.get(r).getQ28();
+                        break;
+                }
+            }
+        }
+
+        // create the training set 
+        MLDataSet training = new BasicMLDataSet(input, null);
+
+        // Create the neural network. 
+        SOM network = new SOM(23, 23);
+        network.reset();
+
+        BasicTrainSOM train = new BasicTrainSOM(
+                network,
+                0.7,
+                training,
+                new NeighborhoodSingle());
+
+        int iteration = 0;
+
+        for (iteration = 0; iteration < 10000; iteration++) {
+            train.iteration();
+        }
+
+//        MLData data1 = new BasicMLData(SOM_INPUT1[0]);
+//        MLData data2 = new BasicMLData(SOM_INPUT1[1]);
+//        System.out.println("Pattern 1 winner: " + network.classify(data1));
+//        System.out.println("Pattern 2 winner: " + network.classify(data2));
+
+        Matrix m = network.getWeights();
+        for (int i = 0; i < m.getRows(); i++) {
+            System.out.print("[");
+            for (int j = 0; j < m.getCols(); j++) {
+                System.out.print(m.get(i, j) + "\t");
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+        Encog.getInstance().shutdown();
     }
 }
